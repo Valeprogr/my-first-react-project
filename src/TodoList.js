112baf0894d1449ab30ad85e10b733ca9f52d7e6
valeviit <valeviit@icloud.com>
2022-10-29 16:53:46 +0200

    add skeleton of the Todo list

---
+++ src/TodoList.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import Todo from "./Todo";
+
+
+const TodoList = ({todos}) => {
+    return (
+       
+            todos.map(todo =>{
+                return <Todo key={todo} todo={todo}/>
+            })
+      
+    );
+}
+
+export default TodoList;
